a476a4f931b2274bbf0994f6917b9860e48c2269
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2025-09-22 00:05:40 -0600

    Add interfaces for campeones and campeonesEscuadra

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,23 @@
+export interface campeonesInterface{
+   idBanda: number;
+    
+    nombreBanda: string;
+    posicion: number;
+    fechaEvento: string;
+    imagenBandaURL: string;
+    federacion: string;
+    categoria: string;
+    evento: string;
+
+}
+export interface campeonesEscuadraInteface{
+    idBanda: number;
+    
+    nombreBanda: string;
+    escuadra: string;
+    fechaEvento: string;
+    imagenBandaURL: string;
+    federacion: string;
+    categoria: string;
+
+}
